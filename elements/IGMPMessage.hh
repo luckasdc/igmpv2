68bb4f5e29f1e257c9554c343b9521b5a640cf5b
Stan Schepers <stanpolschepers@gmail.com>
2019-11-05 14:28:57 +0100

    report query

--- elements/IGMPMessage.hh
+++ elements/IGMPMessage.hh
@@ -16,7 +16,18 @@ struct IPHeader {
 struct IGMPMessage {
     uint8_t type;
     uin
+};
 
+
+struct report_query {
+    uint8_t type = 0x22;
+    uint8_t reserved;
+    uint16_t checksum;
+    uint16_t reserved;
+    uint16_t n_group_records;
+    Vector<group_record> group_records;
+
+    inline unsigned int size() { return 8 + (this->n_group_records * sizeof(group_record)) }
 };
 
 
