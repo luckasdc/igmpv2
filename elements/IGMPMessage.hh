7dce599c90c0c026b7cf73a25251d11091c7531e
luckasdc <mail@luckasdc.be>
2019-11-05 14:29:52 +0100

    group_record added

--- elements/IGMPMessage.hh
+++ elements/IGMPMessage.hh
@@ -13,11 +13,26 @@ struct IPHeader {
 
 };
 
-struct IGMPMessage {
+struct IGMPQueryMessage {
     uint8_t type;
-    uin
+    uint8_t max_resp_code;
+    uint16_t checksum;
+    IPAddress group_address;
+    uint8_t resv : 4;
+    uint8_t s_flag : 1;
+    uint8_t qrv : 3;
+    uint8_t qqic;
+    uint8_t n_sources;
+    std::vector<IPAddress> source_addresses;
 };
 
+struct group_record {
+    uint8_t record_type;
+    uint8_t aux_data_len;
+    uint16_t n_sources;
+    IPAddress multicast_address
+    std::vector<IPAddress> source_addresses;
+};
 
 struct report_query {
     uint8_t type = 0x22;
