bd45f74a5b3366c5948537437243a2ea3c137225
luckasdc <mail@luckasdc.be>
2020-01-06 14:28:26 +0100

    Added Scenario

--- run_scenario.sh
+++ run_scenario.sh
@@ -13,55 +13,37 @@ sclient31="write client31/igmp"
 sclient32="write client32/igmp"
 
 # wait a bit before starting
+
 sleep 1
 
-
+# client21 joins
+echo "$sclient21.join $mcaddr" | telnet localhost $pclient21
+sleep 5
 
 # client31 joins
 echo "$sclient31.join $mcaddr" | telnet localhost $pclient31
-sleep 2
+sleep 5
 
-# client32 join
+# client32 joins
 echo "$sclient32.join $mcaddr" | telnet localhost $pclient32
+sleep 5
+
+# client21 leaves
+echo "$sclient21.leave $mcaddr" | telnet localhost $pclient21
 sleep 5
 
 # client32 leaves
 echo "$sclient32.leave $mcaddr" | telnet localhost $pclient32
-sleep 20
+sleep 5
+
+# client22 joins
+echo "$sclient22.join $mcaddr" | telnet localhost $pclient22
+sleep 5
 
 # client31 leaves
 echo "$sclient31.leave $mcaddr" | telnet localhost $pclient31
 sleep 5
 
-
-# client21 joins
-#echo "$sclient21.join $mcaddr" | telnet localhost $pclient21
-#sleep 5
-
-# client31 joins
-#echo "$sclient31.join $mcaddr" | telnet localhost $pclient31
-#sleep 5
-
-# client32 joins
-#echo "$sclient32.join $mcaddr" | telnet localhost $pclient32
-#sleep 5
-
-# client21 leaves
-#echo "$sclient21.leave $mcaddr" | telnet localhost $pclient21
-#sleep 5
-
-# client32 leaves
-#echo "$sclient32.leave $mcaddr" | telnet localhost $pclient32
-#sleep 5
-
-# client22 joins
-#echo "$sclient22.join $mcaddr" | telnet localhost $pclient22
-#sleep 5
-
-# client31 leaves
-#echo "$sclient31.leave $mcaddr" | telnet localhost $pclient31
-#sleep 5
-
 # client22 leaves
-#echo "$sclient22.leave $mcaddr" | telnet localhost $pclient22
-#sleep 5
+echo "$sclient22.leave $mcaddr" | telnet localhost $pclient22
+sleep 5
