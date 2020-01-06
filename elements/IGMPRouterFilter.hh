9683343d1453497706098de3c1940a4cfe8fe0b0
Stan Schepers <stanpolschepers@gmail.com>
2020-01-06 14:39:40 +0100

    fitler

--- elements/IGMPRouterFilter.hh
+++ elements/IGMPRouterFilter.hh
@@ -1,4 +1,3 @@
-#pragma once
 
 #ifndef IGMP_IGMPFILTER_HH
 #define IGMP_IGMPFILTER_HH
@@ -6,6 +5,8 @@
 #include <click/string.hh>
 #include <click/hashtable.hh>
 #include <click/vector.hh>
+
+
 
 class GroupRecord;
 
@@ -17,30 +18,14 @@ enum class FilterMode : unsigned int {
 
 namespace router {
 
-    struct SourceRecord;
+    struct GroupState;
 
-    struct GroupState {
-        IPAddress multicast_address;
-        Timer group_timer;
-        FilterMode filter_mode = FilterMode::INCLUDE;
-        HashTable<IPAddress, SourceRecord*> source_records;
-        bool has_changed = false;
-
-        static void start_group_timer(Timer* timer, void* data);
-
-        void end_group_timer();
-
-        void reception_current_record(GroupRecord* group_record);
-
-        GroupState(IPAddress multicast_address, FilterMode filter_mode, uint timer_ms);
-
-
-    };
 
     struct SourceRecord {
         GroupState* group_state; // pointer
         IPAddress source_address;
         Timer timer;
+        bool finished = false;
 
         static void start_source_record_timer(Timer* timer, void* data);
 
@@ -50,11 +35,43 @@ namespace router {
 
     };
 
+    struct GroupState {
+        IPAddress multicast_address;
+        Timer group_timer;
+        FilterMode filter_mode = FilterMode::INCLUDE;
+
+        HashTable<IPAddress, SourceRecord*> source_records;
+
+        bool has_changed = false;
+
+        static void start_group_timer(Timer* timer, void* data);
+
+        void end_group_timer();
+
+        bool listening(IPAddress);
+
+        void reception_current_record(GroupRecord* group_record);
+
+        Vector<IPAddress> get_excluded_addresses() const {
+            Vector <IPAddress> output;
+            for (const auto &kv:this->source_records) {
+                auto source_record = kv.second;
+                if (source_record->finished) {
+                    output.push_back(source_record->source_address);
+                }
+            }
+            return output;
+        }
+
+        GroupState(IPAddress multicast_address, FilterMode filter_mode, uint timer_ms);
+
+    };
+
     struct RouterState {
         HashTable<int, GroupState*> group_states;
 
 
-        bool listening(IPAddress, IPAddress) const;
+        bool listening(IPAddress, IPAddress);
     };
 
 }
